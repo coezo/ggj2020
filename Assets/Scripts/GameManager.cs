a99efcc3576df7f8a18dcafd9adae9a8ea62b73a
coezo <flicko@gmail.com>
2020-02-02 11:26:36 -0300

    Adicao de limite para o spawn de itens (soh da spawn se nao tiver itens naquela posicao)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
-   
+
     public GameObject hammer;
     public GameObject ironCoin;
     public GameObject wood;
@@ -18,6 +18,7 @@ public class GameManager : MonoBehaviour
     public float range = 0.2f;
 
     private System.Random randomGen;
+    public GameObject itemSpawnPositions;
     private Transform itemPositionsComponent;
 
     private void Awake()
@@ -36,43 +37,53 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         randomGen = new System.Random();
-        itemPositionsComponent = GameObject.Find("ItemSpawPositions").transform;
+        itemPositionsComponent = itemSpawnPositions.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
         spawnTime -= Time.deltaTime;
-        if(spawnTime <= 0)
+        if (spawnTime <= 0)
         {
-           spawnTime = SPAWN_TIME;
-           int children = itemPositionsComponent.childCount;
-           int postionIndex = randomGen.Next(0, children);
+            spawnTime = SPAWN_TIME;
             
-           var position = itemPositionsComponent.GetChild(postionIndex).transform.position;
-           // float xAxis = UnityEngine.Random.Range(position.x - range, position.x + range);
-           // float yAxis = UnityEngine.Random.Range(position.y - range, position.y + range);
-           int rand = randomGen.Next(0, 50);
-           GameObject item = null;
-           if(rand < 5)
-           {
-              item = hammer;
-           }
-           else if (rand < 15)
-           {
-              item = ironCoin;
-           }
-           else if (rand < 35)
-           {
-              item = wood;
-           }
-           else if (rand <= 50)
-           {
-              item = wool;
-           }
+            // float xAxis = UnityEngine.Random.Range(position.x - range, position.x + range);
+            // float yAxis = UnityEngine.Random.Range(position.y - range, position.y + range);
+            int rand = randomGen.Next(0, 50);
+            GameObject item = null;
+            if (rand < 5)
+            {
+                item = hammer;
+            }
+            else if (rand < 15)
+            {
+                item = ironCoin;
+            }
+            else if (rand < 35)
+            {
+                item = wood;
+            }
+            else if (rand <= 50)
+            {
+                item = wool;
+            }
 
-           Instantiate(item, new Vector3(position.x, position.y, 0.0f), Quaternion.identity); 
-        } 
+            SpawnItem(item);
+        }
+    }
+
+    private void SpawnItem(GameObject item)
+    {
+        int children = itemPositionsComponent.childCount;
+        int positionIndex = randomGen.Next(0, children);
+        var positionContainer = itemPositionsComponent.GetChild(positionIndex);
+
+        if (positionContainer.childCount == 0)
+        {
+            var instance = Instantiate(item, positionContainer.transform.position, Quaternion.identity);
+            instance.transform.parent = positionContainer.transform;
+        }
     }
 
 }
